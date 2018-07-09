b476e4f1f6fc3d989653b98829b0730096ed7e85
babriatis <bra29@pitt.edu>
2018-07-09 17:20:01 -0400

    Added change background color functionality

--- number.rb
+++ number.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+GlobalState = {}
 
 # Determine fibonacci number
 def fibonacci(n)
@@ -27,8 +28,9 @@ get '/' do
   fib = params['Fibonacci']
   fac = params['Factorial']
   hi = params['Say hello']
-
-  erb :index, :locals => { fibonacci: fib, factorial: fac, say_hi: hi }
+  color = params['Color']
+  
+  erb :index, :locals => { fibonacci: fib, factorial: fac, say_hi: hi, colored_index: color }
 end
 
 get '/fibonacci' do
@@ -61,6 +63,25 @@ get '/sayhi' do
   erb :say_hi, :locals => { name: uppercase_name }
 end
 
+get '/changecolor' do
+  # Array of all possible HTML color names
+  color_names = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+  
+  # Check for parameter to persist color across refreshes and backreferences
+  unless params.has_key?(:Color)
+    new_color = GlobalState[:new_color]
+    puts 'Inside conditional'
+  else
+    new_color = params['Color']
+  end
+  
+  # Set to default if not in HTML color array
+  unless color_names.include? new_color
+    new_color = 'LightBlue'
+  end
+  GlobalState[:new_color] = new_color
+  erb :colored_index
+end
 
 not_found do
   status 404
